a5f88e1d27d5597a3a83d1b319256659c90f529b
Teemu Tossavainen <teemu.tossavainen@hiq.fi>
2022-08-23 13:06:07 +0300

    doc and cleanup

--- Frends.Regex.IsMatch/Frends.Regex.IsMatch/Regex.cs
+++ Frends.Regex.IsMatch/Frends.Regex.IsMatch/Regex.cs
@@ -2,29 +2,28 @@
 using System;
 using System.ComponentModel;
 
-namespace Frends.Regex.IsMatch
+namespace Frends.Regex.IsMatch;
+
+/// <summary>
+/// Regex IsMatch task.
+/// </summary>
+public static class Regex
 {
     /// <summary>
-    /// Regex IsMatch task.
+    /// Reads text and returns a boolean indicating if the text matches with specified regular expression.
+    /// [Documentation](https://tasks.frends.com/tasks/frends-tasks/Frends.Regex.IsMatch)
     /// </summary>
-    public static class Regex
+    /// <returns>{bool IsMatch}</returns>
+    public static Result IsMatch([PropertyTab] Input input)
     {
-        /// <summary>
-        /// Reads text and returns a boolean indicating if the text matches with specified regular expression.
-        /// [Documentation](https://tasks.frends.com/tasks/frends-tasks/Frends.Regex.IsMatch)
-        /// </summary>
-        /// <returns>{bool IsMatch}</returns>
-        public static Result IsMatch([PropertyTab] Input input)
+        if (input == null) throw new ArgumentNullException(nameof(input));
+        else
         {
-            if (input == null) throw new ArgumentNullException(nameof(input));
-            else
-            {
-                if (string.IsNullOrEmpty(input.InputText)) throw new ArgumentNullException(nameof(input.InputText));
-                if (string.IsNullOrEmpty(input.RegularExpression)) throw new ArgumentNullException(nameof(input.RegularExpression));
-            }
-
-            var regex = new System.Text.RegularExpressions.Regex(input.RegularExpression);
-            return new Result(regex.IsMatch(input.InputText));
+            if (string.IsNullOrEmpty(input.InputText)) throw new ArgumentNullException(nameof(input.InputText));
+            if (string.IsNullOrEmpty(input.RegularExpression)) throw new ArgumentNullException(nameof(input.RegularExpression));
         }
+
+        var regex = new System.Text.RegularExpressions.Regex(input.RegularExpression);
+        return new Result(regex.IsMatch(input.InputText));
     }
 }
